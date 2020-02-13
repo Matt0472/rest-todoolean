272abf2ebed570cc4c9275da462c178ceaadd548
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-13 16:19:52 +0100

    lista stampata a schermo

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-
+  $.ajax({
+    url: 'http://157.230.17.132:3029/todos',
+    method: 'GET',
+    success: function(data) {
+      var source = $('#entry-template').html();
+      var template = Handlebars.compile(source);
+      for (var i = 0; i < data.length; i++) {
+        var singleElement = data[i].text;
+        var singleId = data[i].id;
+        console.log(singleElement, singleId);
+        var context = {
+          text: singleElement,
+          id: singleId
+        };
+        var html = template(context);
+        $('.todo_list').append(html);
+      }
+    },
+    error: function (request, state, errors) {
+      alert('Errore' + ' ' + error);
+    }
+  });
 });
 
 
