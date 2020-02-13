d5e1f311d3d3d027ce14ea61bad4076a6b82a7cb
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-13 16:33:54 +0100

    inserimento in lista

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $(document).on('click', '#send_btn', function() {
     var inputVal = $('#input_add').val();
     console.log(inputVal);
+    addElement(inputVal);
   });
 });
 
@@ -34,6 +35,29 @@ function printAllList() {
 
 
 function addElement(value) {
-
+  $.ajax({
+    url: 'http://157.230.17.132:3029/todos',
+    method: 'POST',
+    data: {
+      text: value
+    },
+    success: function(data) {
+      var source = $('#entry-template').html();
+      var template = Handlebars.compile(source);
+      for (var i = 0; i < data.length; i++) {
+        var thisElement = data[i].text;
+        var singleId = data[i].id;
+        var context = {
+          text: thisElement,
+          id: singleId
+        };
+        var html = template(context);
+        $('.todo_list').append(html);
+      }
+    },
+    error: function (request, state, errors) {
+      alert('Errore' + ' ' + error);
+    }
+  });
 }
 // http://157.230.17.132:3029/todos
